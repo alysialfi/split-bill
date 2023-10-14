b01c498432310e467b2329c90f6721cab8b5ddbf
Alysia Alfi <alfi@domainesia.com>
2023-10-14 20:16:19 +0700

    feat: vite, reactjs, and tailwindcss installation

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import GrabBill from './components/GrabBill.jsx';
+
+function App() {
+    return (
+        <>
+            <div>
+                <GrabBill />
+            </div>
+        </>
+    )
+}
+
+export default App
